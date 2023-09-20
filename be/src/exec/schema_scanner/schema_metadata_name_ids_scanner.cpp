9ced2fa5d073d77cf0987e504833ec79cf011a30
bobhan1 <bh2444151092@outlook.com>
2023-09-20 16:49:03 +0800

    update

--- be/src/exec/schema_scanner/schema_metadata_name_ids_scanner.cpp
+++ be/src/exec/schema_scanner/schema_metadata_name_ids_scanner.cpp
@@ -144,9 +144,9 @@ Status SchemaMetadataNameIdsScanner::_fill_block_impl(vectorized::Block* block) 
                 srcs[i] = id;
                 datas[i] = srcs + i;
             }
-            fill_dest_column_for_range(block, 0, datas);
+            RETURN_IF_ERROR(fill_dest_column_for_range(block, 0, datas));
         } else {
-            fill_dest_column_for_range(block, 0, null_datas);
+            RETURN_IF_ERROR(fill_dest_column_for_range(block, 0, null_datas));
         }
     }
 
@@ -158,9 +158,9 @@ Status SchemaMetadataNameIdsScanner::_fill_block_impl(vectorized::Block* block) 
             for (int i = 0; i < table_num; ++i) {
                 datas[i] = &str_slot;
             }
-            fill_dest_column_for_range(block, 1, datas);
+            RETURN_IF_ERROR(fill_dest_column_for_range(block, 1, datas));
         } else {
-            fill_dest_column_for_range(block, 1, null_datas);
+            RETURN_IF_ERROR(fill_dest_column_for_range(block, 1, null_datas));
         }
     }
 
@@ -173,9 +173,9 @@ Status SchemaMetadataNameIdsScanner::_fill_block_impl(vectorized::Block* block) 
                 srcs[i] = id;
                 datas[i] = srcs + i;
             }
-            fill_dest_column_for_range(block, 2, datas);
+            RETURN_IF_ERROR(fill_dest_column_for_range(block, 2, datas));
         } else {
-            fill_dest_column_for_range(block, 2, null_datas);
+            RETURN_IF_ERROR(fill_dest_column_for_range(block, 2, null_datas));
         }
     }
 
@@ -187,9 +187,9 @@ Status SchemaMetadataNameIdsScanner::_fill_block_impl(vectorized::Block* block) 
             for (int i = 0; i < table_num; ++i) {
                 datas[i] = &str_slot;
             }
-            fill_dest_column_for_range(block, 3, datas);
+            RETURN_IF_ERROR(fill_dest_column_for_range(block, 3, datas));
         } else {
-            fill_dest_column_for_range(block, 3, null_datas);
+            RETURN_IF_ERROR(fill_dest_column_for_range(block, 3, null_datas));
         }
     }
     //     table_id
@@ -203,7 +203,7 @@ Status SchemaMetadataNameIdsScanner::_fill_block_impl(vectorized::Block* block) 
                 datas[i] = nullptr;
             }
         }
-        fill_dest_column_for_range(block, 4, datas);
+        RETURN_IF_ERROR(fill_dest_column_for_range(block, 4, datas));
     }
 
     //table_name
@@ -218,7 +218,7 @@ Status SchemaMetadataNameIdsScanner::_fill_block_impl(vectorized::Block* block) 
                 datas[i] = nullptr;
             }
         }
-        fill_dest_column_for_range(block, 5, datas);
+        RETURN_IF_ERROR(fill_dest_column_for_range(block, 5, datas));
     }
 
     return Status::OK();
