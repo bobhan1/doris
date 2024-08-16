5dbc37142e0b17359e2095f10fe0fb78c96b1848
bobhan1 <bh2444151092@outlook.com>
2024-08-16 18:40:30 +0800

    rename old partial update to fixed_partial_update

--- fe/fe-core/src/main/java/org/apache/doris/load/routineload/RoutineLoadJob.java
+++ fe/fe-core/src/main/java/org/apache/doris/load/routineload/RoutineLoadJob.java
@@ -702,7 +702,7 @@ public abstract class RoutineLoadJob
     }
 
     @Override
-    public boolean isPartialUpdate() {
+    public boolean isFixedPartialUpdate() {
         return isPartialUpdate;
     }
 
