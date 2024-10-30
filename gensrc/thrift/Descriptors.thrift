9e70f72fe738726f232df401196e302425209493
bobhan1 <bh2444151092@outlook.com>
2024-10-30 12:11:49 +0800

    fix and add flexible update default value case

--- gensrc/thrift/Descriptors.thrift
+++ gensrc/thrift/Descriptors.thrift
@@ -42,6 +42,8 @@ struct TColumn {
     17: optional bool result_is_nullable
     18: optional bool is_auto_increment = false;
     19: optional i32 cluster_key_id = -1
+    20: optional i32 be_exec_version = -1
+    21: optional bool is_on_update_current_timestamp = false
 }
 
 struct TSlotDescriptor {
